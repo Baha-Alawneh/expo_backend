d18885f7a16d79305b517e026b624f08df14cc6f
Baha-Alawneh <bahaalawneh07@gmail.com>
2025-10-13 16:27:23 +0300

    login logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,3 +25,22 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: 'Error registering user' });
     }
 };
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findUserByEmail(email);
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+        const token = jwt.sign({ userId: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(200).json({ token });
+
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ message: 'Error logging in' });
+    };
+};
